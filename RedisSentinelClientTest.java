891b0484b1395e5e2cb5b3edc7284622e26c8aa2
kennykinte <kennykinte@qq.com>
2015-01-29 23:07:23 +0800

    init

---
+++ RedisSentinelClientTest.java
@@ -0,0 +1,80 @@
+package mycode.redis;
+
+import mycode.redis.lib.RedisSentinel;
+import mycode.redis.lib.RedisSentinelJedisPool;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.ShardedJedis;
+
+public class RedisSentinelClientTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		String host = "192.168.1.108";
+		int port = 26379;
+		String clusterName = "mymaster";
+		// RedisSentinel redisSentinelShardedJedisPool = new
+		// RedisSentinelShardedJedisPool(host, port, clusterName);
+		// testSet(redisSentinelShardedJedisPool);
+		RedisSentinel redisSentinelJedisPool = new RedisSentinelJedisPool(host, port, clusterName);
+		setRedisSentinelJedisPool(redisSentinelJedisPool);
+	}
+
+	/**
+	 * 模拟多线程操作
+	 */
+	public static void setRedisSentinelShardedJedisPool(final RedisSentinel client) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					int count = 0;
+					while (true) {
+						ShardedJedis shardedJedis = (ShardedJedis) client.getResource();
+						Thread.sleep(1000);
+						shardedJedis.set("b" + count, "b" + count);
+						client.returnBrokenResource(shardedJedis);
+						count++;
+						System.out.println("set" + count);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					try {
+						Thread.sleep(30000);
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		}).start();
+	}
+
+	/**
+	 * 模拟多线程操作
+	 */
+	public static void setRedisSentinelJedisPool(final RedisSentinel client) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int count = 0;
+				while (true) {
+					Jedis jedis = null;
+					try {
+						jedis = (Jedis) client.getResource();
+						Thread.sleep(1000);
+						jedis.set("b" + count, "b" + count);
+						count++;
+						System.out.println("set" + count);
+					} catch (Exception e) {
+						e.printStackTrace();
+						try {
+							Thread.sleep(30000);
+						} catch (InterruptedException e1) {
+							e1.printStackTrace();
+						}
+					} finally {
+						client.returnBrokenResource(jedis);
+					}
+				}
+			}
+		}).start();
+	}
+}
